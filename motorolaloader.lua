a4a186ffd00496882c1396db5967f96af02c1472
poallini <dhhdgdfedeifkf@gmail.com>
2026-05-08 21:37:17 -0300

    Update motorolaloader.lua

--- motorolaloader.lua
+++ motorolaloader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/poallini/BloxFruitsTP/refs/heads/main/h1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/poallini/BloxFruitsTP/refs/heads/main/oloaloaloaolo.lua"))()
