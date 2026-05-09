b4488f726313eed77e5d242b0eea926d665782ee
poallini <dhhdgdfedeifkf@gmail.com>
2026-05-08 21:41:31 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/poallini/BloxFruitsTP/refs/heads/main/hello.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/poallini/BloxFruitsTP/refs/heads/main/poop.lua"))()
